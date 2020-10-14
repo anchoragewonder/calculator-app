30fc8908a0cee2abb79a44d968a5982e4f7b59d2
anchoragewonder <anchoragewonder@gmail.com>
2020-10-14 12:49:05 -0400

    Add files via upload

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    interface Inumbers
+    {
+        public void getnum();
+    }
+
+    class Program
+    {      
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your go to C# calculator");
+            Console.WriteLine("Enter first number:");
+         
+        }
+    }
+}
---
+++ CalculatorApp/functions/addition.cs
@@ -0,0 +1,26 @@
+﻿using CalculatorApp;
+using System;
+
+public class addition : Inumbers 
+{
+   
+
+    public void getnum()
+    {
+        
+        Console.WriteLine("enter first number:");
+        float num1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter operator,  +  -  *  /  :");
+        char oper = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter second number:");
+        float num2 = float.Parse(Console.ReadLine());
+    }
+    public void add()
+    {
+        Console.WriteLine("Your answer is ")
+
+    }
+}
+	
