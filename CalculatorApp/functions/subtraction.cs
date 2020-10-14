515176fd58806379829e308cddd6daa0b1b641d4
Dan Flanagan <dan.flanagan93@gmail.com>
2020-10-14 13:49:11 -0400

    interface & formatting

--- CalculatorApp/functions/subtraction.cs
+++ CalculatorApp/functions/subtraction.cs
@@ -2,7 +2,7 @@
 
 public class subtract
 {
-	public static float subnum(float first, float second)
+	public float Execute(float first, float second)
 	{
             float answer = first - second;
 
